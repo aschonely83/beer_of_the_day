d82ee3cf22e20ba14c00a861475ae93b0487d20b
Adam Schonely <aschonely@gmail.com>
2019-09-14 11:06:51 -0400

    Fixed issue with scraper method

--- lib/beer_of_the_day/scraper.rb
+++ lib/beer_of_the_day/scraper.rb
@@ -4,7 +4,7 @@ require "nokogiri"
 class BeerOfTheDay::Scraper
   
     def scraper
-      url = https://www.beeroftheday.com/
+      url = "https://www.beeroftheday.com/"
       unparsed_page = HTTParty.get(url)
     end  
 end    
