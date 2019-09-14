79004da6f40002e041221d2ca0240a5311a7c545
Adam Schonely <aschonely@gmail.com>
2019-09-14 15:53:42 -0400

    Fixed an issue in the file tree

---
+++ lib/beer_of_the_day.rb
@@ -0,0 +1,9 @@
+require_relative "beer_of_the_day/version"
+require_relative "beer_of_the_day/cli"
+
+
+
+module BeerOfTheDay
+  class Error < StandardError; end
+  # Your code goes here...
+end
--- lib/beer_of_the_day/beer_of_the_day.rb
+++
@@ -1,9 +0,0 @@
-require_relative "beer_of_the_day/version"
-require_relative "beer_of_the_day/cli"
-
-
-
-module BeerOfTheDay
-  class Error < StandardError; end
-  # Your code goes here...
-end
