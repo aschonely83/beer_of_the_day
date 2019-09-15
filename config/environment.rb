c470224d45bf48222490be4e7c78a470e9158c84
Adam <aschonely@gmail.com>
2019-09-15 17:36:33 -0400

    Updated environment and bin file

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "./beer_of_the_day/scraper"
-require_relative "./beer_of_the_day/beer"
-require_relative "./beer_of_the_day/cli"
-require_relative "./beer_of_the_day/version"
+require_relative "../lib/beer_of_the_day/scraper"
+require_relative "../lib/beer_of_the_day/beer"
+require_relative "../lib/beer_of_the_day/cli"
+require_relative "../lib/beer_of_the_day/version"
