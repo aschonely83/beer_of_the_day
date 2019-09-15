aea64e3d57549359fdfc384f1aaef935122632f3
Adam Schonely <aschonely@gmail.com>
2019-09-14 22:07:42 -0400

    Working on scraper class

--- lib/beer_of_the_day/scraper.rb
+++ lib/beer_of_the_day/scraper.rb
@@ -2,15 +2,13 @@ class BeerOfTheDay::Scraper
     attr_accessor :name, :category, :brewery, :url 
 
     def self.scrape
-      doc = Nokogiri::HTML(open("https://www.beeroftheday.com/"))
+      doc = Nokogiri::HTML(open('https://www.beeroftheday.com'))
 
-      beer = self.new
-      beer.name = doc
-      beer.category = doc
-      beer.brewery = doc
-      beer.url = doc
+      name = doc
+      category = doc
+      brewery = doc
+      url = doc 
       
-      beer 
     end
 
 end
