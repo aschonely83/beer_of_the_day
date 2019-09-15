88ede4343d95336d11bbf86e342df7a47bd339ac
Adam Schonely <aschonely@gmail.com>
2019-09-14 20:30:20 -0400

    Started building my scraper

--- lib/beer_of_the_day/beer.rb
+++ lib/beer_of_the_day/beer.rb
@@ -1,4 +1,14 @@
 class BeerOfTheDay::Beer
     attr_accessor :name, :category, :brewery, :url 
 
-end    
+    def self.scrape_beer
+      doc = Nokogiri::HTML(open("https://www.beeroftheday.com/"))
+
+      beer = self.new
+      beer.name = doc
+      beer.category = doc
+      beer.brewery = doc
+      beer.url = doc
+    end
+
+end
