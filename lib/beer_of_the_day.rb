75e5e0b5b07da956d094231a9e9bf10670763ebd
Adam Schonely <aschonely@gmail.com>
2019-09-14 21:00:10 -0400

    Updated the the two lib beer of the day files

--- lib/beer_of_the_day.rb
+++ lib/beer_of_the_day.rb
@@ -3,5 +3,6 @@ require 'nokogiri'
 require 'pry'
 
 require_relative "./beer_of_the_day/version"
+require_relative "./beer_of_the_day/scraper"
 require_relative "./beer_of_the_day/beer"
 require_relative "./beer_of_the_day/cli"
---
+++ lib/beer_of_the_day/beer.rb
@@ -0,0 +1,5 @@
+require_relative "beer_of_the_day/version"
+
+module BeerOfTheDay
+  class Error < StandardError; end
+end  
