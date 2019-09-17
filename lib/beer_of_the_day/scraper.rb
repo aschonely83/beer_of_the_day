7462bc1d7149a61b3aec2c7df9e25559c296d0d7
Adam <aschonely@gmail.com>
2019-09-17 07:11:35 -0400

    Added separate methods to the scraper class.

--- lib/beer_of_the_day/scraper.rb
+++ lib/beer_of_the_day/scraper.rb
@@ -1,12 +1,16 @@
 module BeerOfTheDay
   class Scraper
 
-    def self.scrape  
+    def self.scrape_name  
       doc = Nokogiri::HTML(open('https://www.beeroftheday.com'))
 
-      name = doc.css('h1.beer_title').text
-      brewery = doc.css('a.dark_link').first.text
-      BeerOfTheDay::Beer.new(name, brewery) 
+      @name = doc.css('h1.beer_title').text
     end
+
+    def self.scrape_brewery
+      doc = Nokogiri::HTML(open('https://www.beeroftheday.com'))
+
+      @brewery = doc.css('a.dark_link').first.text
+    end  
   end
-end
+end    
