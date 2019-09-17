0585a1e7d2cfc1c178a38349e5e7456af2d93144
Adam <aschonely@gmail.com>
2019-09-17 07:03:29 -0400

    updated gemspec file, fixed issue that broke the cli

--- lib/beer_of_the_day/cli.rb
+++ lib/beer_of_the_day/cli.rb
@@ -1,40 +1,26 @@
+ 
 module BeerOfTheDay
   class Cli 
 
     def call
-      list_beer
-      list_brewery
+      puts "Today's Beer Of The Day:"
+      @input = nil
       menu
     end
-    
-    def list_beer
-      puts "Today's Beer Of The Day:"
-      @name = BeerOfTheDay::Scraper.scrape
-    end
-    
-    def list_brewery
-      puts "Today's Brewery Of The Day:"
-      @brewery = BeerOfTheDay::Scraper.scrape
-    end
-    
-
+  
     def menu
-      @input = nil
-      while @input != "exit" 
-        puts "Enter the number you want would like more info about."
-        @input = gets.strip
-      
       puts <<-MENU
       1. Beer Name
       2. Brewery Name
       type "exit" at any time to leave program.
       MENU
-     
+      while @input != "exit" 
+      @input = gets.strip
       case @input
-      when @input == "1"
-        list_beer
-      when @input == "2"
-        list_brewery
+      when "1"
+        name
+      when "2"
+        brewery
       when "exit" 
         puts "Check back tomorrow for the new beer of the day."
       break
