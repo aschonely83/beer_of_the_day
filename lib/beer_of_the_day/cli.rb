ce27d4f65920aba09392abd2f7daea4de65ce346
Adam <aschonely@gmail.com>
2019-09-17 07:33:44 -0400

    Fixed an issue in the scraper. Refactored the menu in the cli.

--- lib/beer_of_the_day/cli.rb
+++ lib/beer_of_the_day/cli.rb
@@ -3,31 +3,38 @@ module BeerOfTheDay
   class Cli 
 
     def call
-      puts "Today's Beer Of The Day:"
+      BeerOfTheDay::Scraper.scrape
+      puts "" 
+      puts "Welcome to Today's Beer Of The Day:"
       @input = nil
       menu
+      while @input != "exit" 
+        @input = gets.strip
+        if  @input == "1"
+          puts "This is the beer of the day:"
+        elsif @input == "2"
+          puts "This is the brewery that makes the beer of the day:"
+        elsif @input == "menu"
+          menu
+        elsif == "exit" 
+          puts "Check back tomorrow for the new beer of the day."
+        break
+        else
+          puts "Not a valid option, select 1-3, or exit "
+        end
+      end
     end
   
     def menu
+      puts "Select an option to get more information."
+      puts ""
       puts <<-MENU
       1. Beer Name
       2. Brewery Name
       type "exit" at any time to leave program.
+      type "menu" to return to the menu at any time.
       MENU
-      while @input != "exit" 
-      @input = gets.strip
-      case @input
-      when "1"
-        name
-      when "2"
-        brewery
-      when "exit" 
-        puts "Check back tomorrow for the new beer of the day."
-      break
-      else
-        puts "Not a valid option, select 1-3, or exit "
-      end
-      end
+      puts ""
     end
   end          
 end
--- lib/beer_of_the_day/scraper.rb
+++ lib/beer_of_the_day/scraper.rb
@@ -1,16 +1,12 @@
 module BeerOfTheDay
   class Scraper
 
-    def self.scrape_name  
+    def self.scrape  
       doc = Nokogiri::HTML(open('https://www.beeroftheday.com'))
 
       @name = doc.css('h1.beer_title').text
-    end
-
-    def self.scrape_brewery
-      doc = Nokogiri::HTML(open('https://www.beeroftheday.com'))
-
       @brewery = doc.css('a.dark_link').first.text
-    end  
+      BeerOfTheDay::Beer.new(name, brewery)
+    end
   end
 end    
